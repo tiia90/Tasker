dbbe5e59231ecadfb15e1fd0949bc3ae3130e4dd
tiia90 <62059457+tiia90@users.noreply.github.com>
2022-04-17 17:30:47 +0300

    lisäyksiä

--- src/app/yourschedule/yourschedule.page.ts
+++ src/app/yourschedule/yourschedule.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AlertController, LoadingController } from '@ionic/angular';
+import { AlertController, LoadingController, ModalController } from '@ionic/angular';
 import { AuthService } from '../services/auth.service';
 
 @Component({
@@ -22,11 +22,15 @@ export class YourschedulePage implements OnInit {
   
   }]
 
+
+ 
+ 
   constructor(  
     private loadingController: LoadingController,
     private alertController: AlertController,
     private authService: AuthService,
-    private route: Router) { }
+    private route: Router,
+    public modalCtrl:ModalController) { }
   ngOnInit(): void {
   }
 
@@ -39,7 +43,38 @@ export class YourschedulePage implements OnInit {
     this.route.navigateByUrl('/login', { replaceUrl: true });
   }
 
-  today : number = Date.now()
+today : number = Date.now()
+
+//tämä jälkeen kaikki on kesken
+
+taskName
+taskDate
+taskDetails
+taskObject
+
+  async dismiss(){
+    await this.modalCtrl.dismiss(this.taskObject)
+  }
+
+  AddTask(){
+    this.taskObject = ({itemName:this.taskName,
+                        itemDate:this.taskDate,
+                      itemDetails:this.taskDetails})
+
+    this.dismiss()
+  }
+   async addTask(){
+     const modal = await this.modalCtrl.create({
+       component: this.AddTask
+     })
+
+     modal.onDidDismiss().then(newTaskObject =>{
+       console.log(newTaskObject.data);
+       this.todoList.push(newTaskObject.data)
+     })
+     return await modal.present()
+   }
+   
 
 }
  
